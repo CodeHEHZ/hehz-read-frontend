5447c2119ea383d4c8d2c88266fcaec1e2d78711
IncredibLink <incrediblelink@gmail.com>
2017-04-01 21:11:24 +0800

    改进 Navbar 并增加 Logout.vue

---
+++ src/components/Logout.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+    export default {
+        created() {
+            this.$cookie.delete('user')
+            this.$cookie.delete('username')
+            this.$cookie.delete('group')
+            this.$http.get(this.$store.state.api + 'user/logout', { credentials: true }).then(response => {
+                if (response.status === 200) {
+                    this.$router.push('/')
+                }
+            })
+        }
+    }
+</script>
